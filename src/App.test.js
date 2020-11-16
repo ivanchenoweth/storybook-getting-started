a3d4f5c7a4806c6738d5e5bf5f8a2d315cf00c34
Ivan Chenoweth <ivanchenoweth@gmail.com>
2020-11-15 22:08:47 -0700

    input-component fix test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Edit/i);
+  const linkElement = screen.getByText(/Review/i);
   expect(linkElement).toBeInTheDocument();
 });
